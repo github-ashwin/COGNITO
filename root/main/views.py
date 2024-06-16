8c48327beac8067734f858b5d55cf29291d272a4
github-ashwin <ashi.github@gmail.com>
2024-06-16 11:13:09 +0530

    Added Create/Update Room Style

--- root/main/views.py
+++ root/main/views.py
@@ -115,15 +115,21 @@ def user_profile(request,pk):
 @login_required(login_url='login')
 def create_room(request):
     form = RoomForm()
+    topics = Topic.objects.all()
     if request.method == 'POST':
-        form = RoomForm(request.POST)
-        if form.is_valid():
-            room = form.save(commit=False)
-            room.host = request.user
-            room.save()
-            return redirect('home')
+
+        topic_name = request.POST.get('topic')
+        topic, created  = Topic.objects.get_or_create(name=topic_name)
+
+        Room.objects.create(
+            host = request.user,
+            topic = topic,
+            name = request.POST.get('name'),
+            description = request.POST.get('description'),
+        )
+        return redirect('home')
         
-    context = {'form': form}
+    context = {'form': form, 'topics':topics}
     return render(request, 'main/create_room.html', context)
 
 # View for updating an existing room
@@ -131,17 +137,21 @@ def create_room(request):
 def update_room(request, pk):
     room = get_object_or_404(Room, id=pk)
     form = RoomForm(instance=room)
+    topics = Topic.objects.all()
 
     if request.user != room.host:
         return HttpResponse('You are not allowed here!')
 
     if request.method == 'POST':
-        form = RoomForm(request.POST, instance=room)
-        if form.is_valid():
-            form.save()
-            return redirect('home')  
+        topic_name = request.POST.get('topic')
+        topic, created  = Topic.objects.get_or_create(name='topic_name')
+        room.name = request.POST.get('name')
+        room.topic = topic
+        room.description = request.POST.get('description')
+        room.save()
+        return redirect('home')  
        
-    context = {'form': form}
+    context = {'form': form, 'topics':topics, 'room':room}
     return render(request, 'main/create_room.html', context)
 
 # View for deleting a room
