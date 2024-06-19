f3b8b7234373c95e2044467188eb418510bb50bd
github-ashwin <ashi.github@gmail.com>
2024-06-19 19:06:18 +0530

    Corrected Login/Signup page

--- root/main/forms.py
+++ root/main/forms.py
@@ -15,7 +15,7 @@ class RoomForm(ModelForm):
 class MyUserCreationForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'password1', 'password2']
 
 
 class UserForm(ModelForm):
