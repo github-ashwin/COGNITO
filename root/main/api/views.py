98d621306126a9bcf32a50b11910d0624daa34a7
github-ashwin <ashi.github@gmail.com>
2024-06-23 19:57:59 +0530

    Added django rest framework

--- root/main/api/views.py
+++ root/main/api/views.py
@@ -1,10 +1,11 @@
-from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-
+@api_view(['GET'])
 def getRoutes(request):
     routes = [
         'GET /api',
         'GET /api/rooms',    # To get all the rooms
         'GET /api/rooms/:id' # To get a specific room 
     ]
-    return JsonResponse(routes, safe=False)
+    return Response(routes)
